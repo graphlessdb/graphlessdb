62bdb0a7c2ff89ff37c6d3eb2517f2fa861ac6c9
Tom Mitchell <tmitchel2@googlemail.com>
2025-11-16 07:26:02 +0800

    Add placeholder test file for RDFTripleGraphQueryService (#36)

---
+++ src/GraphlessDB.Tests/Graph.Services.Internal.Tests/RDFTripleGraphQueryServiceTests.cs
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Small Trading Company Ltd (Destash.com).
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GraphlessDB.Graph.Services.Internal.Tests
+{
+    [TestClass]
+    public sealed class RDFTripleGraphQueryServiceTests
+    {
+        [TestMethod]
+        public void PlaceholderTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
