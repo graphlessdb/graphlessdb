cafbd4104586bdb58a64808bc14bbce707130bb3
Tom Mitchell <tmitchel2@googlemail.com>
2025-11-16 08:38:24 +0000

    Add unit tests for RDFTripleKey
    - Created comprehensive test suite for RDFTripleKey
    - Tests cover CompareTo method and all comparison operators (<, >, <=, >=)
    - Achieved 100% code coverage (14/14 lines covered)
    - All 16 tests passing
    
    Closes #62

---
+++ src/GraphlessDB.Tests/Storage.Tests/RDFTripleKeyTests.cs
@@ -0,0 +1,193 @@
+/**
+ * Copyright (c) Small Trading Company Ltd (Destash.com).
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+using System;
+using GraphlessDB.Storage;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GraphlessDB.Storage.Tests
+{
+    [TestClass]
+    public sealed class RDFTripleKeyTests
+    {
+        [TestMethod]
+        public void CanCompareToWithEqualKeys()
+        {
+            var key1 = new RDFTripleKey("subject1", "predicate1");
+            var key2 = new RDFTripleKey("subject1", "predicate1");
+
+            var result = key1.CompareTo(key2);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void CanCompareToWithLessThanBySubject()
+        {
+            var key1 = new RDFTripleKey("subject1", "predicate1");
+            var key2 = new RDFTripleKey("subject2", "predicate1");
+
+            var result = key1.CompareTo(key2);
+
+            Assert.IsTrue(result < 0);
+        }
+
+        [TestMethod]
+        public void CanCompareToWithGreaterThanBySubject()
+        {
+            var key1 = new RDFTripleKey("subject2", "predicate1");
+            var key2 = new RDFTripleKey("subject1", "predicate1");
+
+            var result = key1.CompareTo(key2);
+
+            Assert.IsTrue(result > 0);
+        }
+
+        [TestMethod]
+        public void CanCompareToWithLessThanByPredicate()
+        {
+            var key1 = new RDFTripleKey("subject1", "predicate1");
+            var key2 = new RDFTripleKey("subject1", "predicate2");
+
+            var result = key1.CompareTo(key2);
+
+            Assert.IsTrue(result < 0);
+        }
+
+        [TestMethod]
+        public void CanCompareToWithGreaterThanByPredicate()
+        {
+            var key1 = new RDFTripleKey("subject1", "predicate2");
+            var key2 = new RDFTripleKey("subject1", "predicate1");
+
+            var result = key1.CompareTo(key2);
+
+            Assert.IsTrue(result > 0);
+        }
+
+        [TestMethod]
+        public void CanCompareToWithNull()
+        {
+            var key1 = new RDFTripleKey("subject1", "predicate1");
+
+            var result = key1.CompareTo(null);
+
+            Assert.IsTrue(result > 0);
+        }
+
+        [TestMethod]
+        public void CanUseLessThanOperator()
+        {
+            var key1 = new RDFTripleKey("subject1", "predicate1");
+            var key2 = new RDFTripleKey("subject2", "predicate1");
+
+            var result = key1 < key2;
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanUseLessThanOperatorReturnsFalse()
+        {
+            var key1 = new RDFTripleKey("subject2", "predicate1");
+            var key2 = new RDFTripleKey("subject1", "predicate1");
+
+            var result = key1 < key2;
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CanUseGreaterThanOperator()
+        {
+            var key1 = new RDFTripleKey("subject2", "predicate1");
+            var key2 = new RDFTripleKey("subject1", "predicate1");
+
+            var result = key1 > key2;
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanUseGreaterThanOperatorReturnsFalse()
+        {
+            var key1 = new RDFTripleKey("subject1", "predicate1");
+            var key2 = new RDFTripleKey("subject2", "predicate1");
+
+            var result = key1 > key2;
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CanUseLessThanOrEqualOperator()
+        {
+            var key1 = new RDFTripleKey("subject1", "predicate1");
+            var key2 = new RDFTripleKey("subject2", "predicate1");
+
+            var result = key1 <= key2;
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanUseLessThanOrEqualOperatorWithEqualKeys()
+        {
+            var key1 = new RDFTripleKey("subject1", "predicate1");
+            var key2 = new RDFTripleKey("subject1", "predicate1");
+
+            var result = key1 <= key2;
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanUseLessThanOrEqualOperatorReturnsFalse()
+        {
+            var key1 = new RDFTripleKey("subject2", "predicate1");
+            var key2 = new RDFTripleKey("subject1", "predicate1");
+
+            var result = key1 <= key2;
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CanUseGreaterThanOrEqualOperator()
+        {
+            var key1 = new RDFTripleKey("subject2", "predicate1");
+            var key2 = new RDFTripleKey("subject1", "predicate1");
+
+            var result = key1 >= key2;
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanUseGreaterThanOrEqualOperatorWithEqualKeys()
+        {
+            var key1 = new RDFTripleKey("subject1", "predicate1");
+            var key2 = new RDFTripleKey("subject1", "predicate1");
+
+            var result = key1 >= key2;
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanUseGreaterThanOrEqualOperatorReturnsFalse()
+        {
+            var key1 = new RDFTripleKey("subject1", "predicate1");
+            var key2 = new RDFTripleKey("subject2", "predicate1");
+
+            var result = key1 >= key2;
+
+            Assert.IsFalse(result);
+        }
+    }
+}
