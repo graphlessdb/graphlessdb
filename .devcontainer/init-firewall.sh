e40e748e535af404c43b983aeb72195c3e9b0062
Tom Mitchell <tmitchel2@googlemail.com>
2025-11-22 11:53:19 +0000

    Update

--- .devcontainer/init-firewall.sh
+++ .devcontainer/init-firewall.sh
@@ -65,6 +65,7 @@ done < <(echo "$gh_ranges" | jq -r '(.web + .api + .git)[]' | aggregate -q)
 
 # Resolve and add other allowed domains
 for domain in \
+    "api.nuget.org" \
     "registry.npmjs.org" \
     "api.anthropic.com" \
     "sentry.io" \
