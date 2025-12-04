482134ac147a15119734a51611121e979e8d04c0
Tom Mitchell <tmitchel2@googlemail.com>
2025-12-04 23:59:42 +0000

    Update code cov

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,7 @@
+cd "$(dirname "$0")"
+
 rm -r ./bin/test/results
 dotnet test --collect:"XPlat Code Coverage" --settings:"settings.runsettings" --filter:"TestCategory!=Integration" --results-directory:"./bin/test/results" ./GraphlessDB.sln
 dotnet reportgenerator -reports:"./bin/test/results/**/coverage.cobertura.xml" -targetdir:"./bin/test/results/codecoverage/"
+
+echo "$(pwd)/bin/test/results/codecoverage/index.html"
