086d408d9a706a6be2781f755801633658005d88
Tom Mitchell <tmitchel2@googlemail.com>
2025-12-05 00:11:34 +0000

    Update build

--- src/build.sh
+++ src/build.sh
@@ -2,6 +2,8 @@ cd "$(dirname "$0")"
 
 rm -r ./bin/test/results
 dotnet test --collect:"XPlat Code Coverage" --settings:"settings.runsettings" --filter:"TestCategory!=Integration" --results-directory:"./bin/test/results" ./GraphlessDB.sln
+
+dotnet tool restore
 dotnet reportgenerator -reports:"./bin/test/results/**/coverage.cobertura.xml" -targetdir:"./bin/test/results/codecoverage/"
 
-echo "$(pwd)/bin/test/results/codecoverage/index.html"
+echo "file://$(pwd)/bin/test/results/codecoverage/index.html"
